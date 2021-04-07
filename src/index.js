a547bb3486d3abaf3b7b0edb721b66597faa54a4
Lautaro Cepeda <lautac29@gmail.com>
2021-04-07 18:04:06 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React        from 'react';
+import ReactDOM     from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Constants
+const GREETING = 'Hola';
+
+
+const user = {
+    firstName: 'Lautaro',
+    lastName: 'Cepeda'
+};
+
+
+function formatName(user) {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+function getGreeting(user) {
+    if ( user ) {
+        return `${ GREETING }, ${ formatName(user) }!`;
+    }
+
+    return `${ GREETING }, Unknown!`;
+}
+
+
+const element = (
+    <div>
+        <h1 style={{ color: 'grey' }}> { getGreeting(user) } </h1>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+
+
+
+
+
+ReactDOM.render(element, container);
