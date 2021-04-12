4a6c7f29703308df65f3234d087d4ef39d66aee8
Lautaro Cepeda <lautac29@gmail.com>
2021-04-12 16:54:10 -0300

    First component Card

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 import React        from 'react';
 import ReactDOM     from 'react-dom';
+import Card         from './components/Card';
 
-// Constants
-const GREETING = 'Hola';
-
-
-const user = {
-    firstName: 'Lautaro',
-    lastName: 'Cepeda'
-};
-
-
-function formatName(user) {
-    return `${user.firstName} ${user.lastName}`;
-}
-
-function getGreeting(user) {
-    if ( user ) {
-        return `${ GREETING }, ${ formatName(user) }!`;
-    }
-
-    return `${ GREETING }, Unknown!`;
-}
-
-
-const element = (
-    <div>
-        <h1 style={{ color: 'grey' }}> { getGreeting(user) } </h1>
-    </div>
-);
 
 const container = document.getElementById('root');
 
 
 
-
-
-ReactDOM.render(element, container);
+ReactDOM.render(<Card></Card>, container);
